ffa9f3eda17e220b2b25933bab11c094611d458f
DARANPHOP SAMUTKAO <6431503022@lamduan.mfu.ac.thcd>
2024-06-20 20:00:48 +0700

    เว้นหน้านี้ไว้ก่อน ;p

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className=" text-white flex items-center justify-center min-h-screen bg-gradient-to-r from-teal-400 to-green-400">
+      
+        <p>Homepage</p>
+        
+    </div>
+  )
+}
