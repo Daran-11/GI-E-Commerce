25ae65f055aa8bd2532610e0833c9f53005ae4e7
DARANPHOP SAMUTKAO <6431503022@lamduan.mfu.ac.thcd>
2024-06-20 22:02:16 +0700

    ใส่ปุ่ม login ชั่วคราวในหน้าหลัก

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 
 export default function Home() {
   return (
-    <div className=" text-white flex items-center justify-center min-h-screen bg-gradient-to-r from-teal-400 to-green-400">
-      
-        <p>Homepage</p>
+    <div className=" text-white flex items-center justify-center   h-screen bg-gradient-to-r from-teal-400 to-green-400">
+        <div className=" hover:text-blue-500">
+            <a href ="/Login">Login click</a>
+        </div>
+
+       
+    
+
         
     </div>
   )
