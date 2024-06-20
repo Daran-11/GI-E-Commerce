7cddd0ce807167bb68887ebef9a6d013962e56ca
DARANPHOP SAMUTKAO <6431503022@lamduan.mfu.ac.thcd>
2024-06-20 19:30:35 +0700

    ใส่ comment

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import ProductCard from "./components/productcard";
 
-
+// หน้าหลัก
+// fetchproduct ดึงข้อมูล ID Name Price ของสินค้ามา
 async function fetchProducts() {
   const res = await fetch('http://localhost:3000/api/product?fields=ProductID,ProductName,Price');
   if (!res.ok) {
@@ -14,8 +15,8 @@ async function fetchProducts() {
 export default async function Home() {
   const products = await fetchProducts();
   return (
-    /* อย่าลบตัว main ไม่งั้น navbar จะไม่ขึ้น เพราะตั้งเอาไว้ใน layout.js */
-    <>     
+    
+    <>
     
     <div className="image bg-pineapple w-full h-[430px] bg-no-repeat bg-cover bg-scroll bg-center ">
     </div>
