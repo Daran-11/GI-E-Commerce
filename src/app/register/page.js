52e6a5c8abc13b5e37e7047d576f5abb5acee9b4
DARANPHOP SAMUTKAO <6431503022@lamduan.mfu.ac.thcd>
2024-06-22 01:52:19 +0700

    ปรับย้ายตัว validate input field แยกออกมาจาก page.js

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -3,6 +3,14 @@
 
 import { useState } from 'react';
 
+import {
+    validateConfirmPassword,
+    validateEmail,
+    validateName,
+    validatePassword,
+    validatePhone
+} from '../components/formValidation';
+
 function RegisterPage() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -15,75 +23,7 @@ function RegisterPage() {
     const [confirmPasswordError, setConfirmPasswordError] = useState("");
     const [phoneError, setPhoneError] = useState("");
 
-    const validateName = (name) => {
-        if (!name) {
-            return "โปรดกรอกชื่อ";
-        }
-        return "";
-    };
 
-    const validateEmail = (email) => {
-       let regex = /^[a-zA-Z0-9_.±]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
-        if(regex.test(email) == false) {
-            return "โปรดกรอกอีกเมลให้ถูกต้อง"
-        }
-
-
-        if (!email.includes('@')) {
-            return "อีเมลต้องมี '@' เช่น gipine@gmail.com";
-        }
-        if (!email.includes('.')) {
-            return "อีเมลต้องมี '.' เช่น gipine@gmail.com";
-        }
-        return "";
-    };
-
-    const validatePassword = (password) => {
-        if (!password) {
-            return "โปรดกรอกรหัสผ่าน";
-        }
-        return "";
-    };
-
-    const validateConfirmPassword = (confirmPassword, password) => {
-        if (!confirmPassword) {
-            return "โปรดยืนยันรหัสผ่าน"
-        }
-        
-        if (confirmPassword !== password) {
-            return "รหัสผ่านไม่ตรงกัน";
-        }
-        return "";
-    };
-
-    const validatePhone = (phone) => {
-        let regex = /^[a-zA-Zก-ฮ]+$/;
-        const checkChar = (phone) => {
-            for (let i = 0; i < phone.length; i++) {
-                // Check if the character is alphanumeric
-                if (/[a-zA-Zก-ฮ]/.test(phone[i])) {
-                    return true; // Return true if any character is a letter
-                }
-            }
-            return false; // Return false if no alphabetic character is found            
-        } 
-        if (checkChar(phone)==true) {
-            return "เบอร์โทรไม่สามารถมีตัวอักษร"
-        }
-        
-
-        if (!phone) {
-            return "โปรดกรอกเบอร์โทร 10 หลัก"
-        } 
-
-        if (regex.test(phone) || !phone) {
-            return "โปรดกรอกเบอร์โทร";            
-        }
-        if (phone.length < 10) {
-            return "โปรดกรอกเลขเบอร์โทรให้ครบถ้วน 10 หลัก"            
-        }
-        return "";
-    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -223,7 +163,7 @@ function RegisterPage() {
                         type="tel"
                         name="phone"
                         placeholder="กรอกเบอร์โทร"
-                        maxlength={10}
+                        maxLength={10}
                         required
                     />
                     {phoneError && <div className="input-error">{phoneError}</div>}                        
