b583dfae6b9fc7aaca3361e915c3982c6964857f
DARANPHOP SAMUTKAO <6431503022@lamduan.mfu.ac.thcd>
2024-07-09 16:42:36 +0700

    ใส่ counter สำหรับเพิ่มลดจำนวนสินค้าสำหรับสั่งซื้อ

---
+++ src/components/counter.js
@@ -0,0 +1,39 @@
+"use client"
+import { useState } from 'react'
+
+export default function Counter({productAmount}) {
+    const [count,setCount] = useState(1)
+
+    const decrement = () => {
+        if (count > 1) {
+            setCount(count - 1)
+        }
+    }
+
+    const increment = () => {
+        if (count < productAmount) {
+            setCount(count + 1)            
+        }
+
+    }
+
+  return (
+
+    
+    <div className=''>
+        <div className='flex place-items-center '>
+            <button className='btn w-10 h-10 border-2' onClick={decrement}>
+                -
+            </button>
+            <h1 className='w-8 text-center'>
+                {count}                 
+            </h1>
+
+            <button className='btn w-10 h-10 border-2' onClick={increment}>
+                +
+            </button>            
+        </div>
+
+    </div>
+  )
+}
