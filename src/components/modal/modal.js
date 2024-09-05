0bede3dc5f6d7816bbef94ad912007ed58343480
DARANPHOP SAMUTKAO <6431503022@lamduan.mfu.ac.thcd>
2024-09-05 23:30:30 +0700

    ทำ component modal เปล่า

---
+++ src/components/modal/modal.js
@@ -0,0 +1,20 @@
+import styles from '../modal/modal.module.css'; // Import CSS for styling the modal
+
+const Modal = ({ children, isOpen, onClose }) => {
+  if (!isOpen) return null; // Don't render if modal is closed
+
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.modal}>
+        <button className={styles.closeButton} onClick={onClose}>
+          &times;
+        </button>
+        <div className={styles.modalContent}>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
