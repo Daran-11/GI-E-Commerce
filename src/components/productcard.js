9f24ee07ff33327b86a1b28b3a4f6519a480b234
DARANPHOP SAMUTKAO <6431503022@lamduan.mfu.ac.thcd>
2024-09-16 03:46:00 +0700

    เพิ่มรูปในตะกร้า ปรับ UI

--- src/components/productcard.js
+++ src/components/productcard.js
@@ -4,13 +4,13 @@ import Link from 'next/link';
 import AddToCartButton from './addToCartButton';
 const ProductCard = ({products}) => {
     return (
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 lg:gap-[30px] xl:gap-[30px] ">
+      <div className="grid grid-cols-2  md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5  lg:gap-[30px] xl:gap-[30px]  gap-[20px]">
         {products.map((product) => (
           
-              <div key={product.ProductName} className="Card flex-col justify-center w-full h-[350px] md:h-[380px] lg:h-[350px] xl:h-full rounded-2xl bg-[#FBFBFB] shadow-lg transition duration-500 ease-in-out transform hover:scale-105 ">
+              <div key={product.ProductName} className="Card flex-col justify-center w-full h-fit lg:h-[350px] xl:h-full rounded-2xl pb-4 bg-[#FBFBFB] shadow-lg transition duration-500 ease-in-out transform hover:scale-105 ">
                   <Link key={product.ProductID} href={`/product/${product.ProductID}${product.ProductName}${product.ProductType}`}>
 
-                  <div className="w-full h-[200px] justify-center">
+                  <div className="w-full h-[125px] xl:h-[200px] justify-center">
                   {product.imageUrl ? (
                     <Image
                       src={product.imageUrl}
@@ -27,23 +27,23 @@ const ProductCard = ({products}) => {
                   </div>
 
                   <div className="px-[18px] h-[70px]">
-                      <div className="text-2xl text-[#535353] mt-3  ">
-                          <p>{product.ProductName} {product.ProductType} </p>
+                      <div className="text-lg sm:text-xl xl:text-2xl text-[#535353] lg:mt-3  ">
+                          <p>{product.ProductName}{product.ProductType} </p>
                       </div>
-                      <div className="mt-[2px] text-[#767676] text-xl ">
+                      <div className="mt-[2px] text-[#767676] text-base xl:text-lg ">
                           <p>ผู้ขาย {product.farmer.farmerName} </p>                    
                       </div>
                   </div>
                   </Link> 
 
-                  <div className= 'xl:flex md:grid md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-2  px-[18px] pt-[10px] text-[#4eac14] flex justify-between items-center border-t-[1px]'>
-                      <p className=' sm:text-[19px]  md:text-[25px]  lg:text-[23px] xl:text-[25px]'> ฿{Number(product.Price).toLocaleString()}</p>     
-                      <p>/กิโลกรัม</p>      
+                  <div className= 'flex md:grid  grid-cols-2 xl:grid-cols-2  px-[18px] lg:pt-[10px] text-[#4eac14] justify-between items-center border-t-[1px]'>
+                      <p className=' text-[18px]  sm:text-[19px]  md:text-[25px]  lg:text-[23px] xl:text-[25px]'> ฿{Number(product.Price).toLocaleString()}</p>     
+                      <p className=' text-end'>/กิโลกรัม</p>      
 
 
 
                   </div>
-                  <div className='px-[18px] mt-[10px] mb-[10px]'>
+                  <div className='px-[18px] mt-[2px] lg:mt-[5px] lg:mb-[10px]'>
 
                   <AddToCartButton product={product}/>  
 
