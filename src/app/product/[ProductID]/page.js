2417f22fbb37cc58f4a99339e8a8c686c7f0ee40
golddlogz78 <6431503027@lamduan.mfu.ac.th>
2024-09-25 16:20:42 +0700

    test

--- src/app/product/[ProductID]/page.js
+++ src/app/product/[ProductID]/page.js
@@ -18,42 +18,7 @@ export async function generateStaticParams() {
 export default async function ProductDetails({ params }) {
   const product = await prisma.product.findUnique({
     where: {
-<<<<<<< HEAD
-      ProductID: parseInt(params.ProductID, 10),
-    },
-    select: {
-      ProductID: true,
-      ProductName: true,
-      ProductType: true,
-      Amount: true,
-      Price: true,
-      Description:true,
-      farmer: {
-        select: {
-          id:true,
-          farmerName: true,  // Select specific fields from the Farmer model
-          location: true,
-          contactLine: true,  // You can include more fields as needed
-        },
-      },
-      reviews: {
-        take: 5, // Limit the number of reviews fetched initially
-        select: {
-          id: true,
-          rating: true,
-          review: true,
-          createdAt: true,
-          user: {
-            select: {
-              id: true,
-              name: true, // Fetch the user who wrote the review
-            }
-          },
-        },
-      },
-=======
       ProductID: parseInt(params.ProductID, 10), // Ensure ProductID is an integer
->>>>>>> parent of 93831e9 (pick from 58b13bcf new)
     },
   });
 
@@ -64,13 +29,7 @@ export default async function ProductDetails({ params }) {
   return (
     <main>
       <div className="top-container">
-<<<<<<< HEAD
-        <ProductDetailsClient product={product}
-          ProductID = {product.ProductID}
-         totalReviewsCount={totalReviewsCount} />
-=======
         <ProductDetailsClient product={product} />
->>>>>>> parent of 93831e9 (pick from 58b13bcf new)
       </div>
     </main>
   );
