eb12ef0619f4b66fb643a4d3524d8174287930a3
DARANPHOP SAMUTKAO <6431503022@lamduan.mfu.ac.thcd>
2024-10-23 01:33:26 +0700

    some ui changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="bg-gray-800 text-white py-4 text-center">
+        <footer className=" h-fit border-t  text-[#535353] py-2 text-center mt-3">
             <p>&copy; 2024 GI Pineapple. All rights reserved.</p>
         </footer>
     );
