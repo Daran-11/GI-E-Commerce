66065eff6ae95dbf28d34b7063ab71404797ecd0
DARANPHOP SAMUTKAO <6431503022@lamduan.mfu.ac.thcd>
2024-10-30 10:45:35 +0700

    seed file for provices amphoes tambons

---
+++ prisma/seed.js
@@ -0,0 +1,33 @@
+// prisma/seed.js
+const { PrismaClient } = require('@prisma/client');
+const fs = require('fs');
+const path = require('path');
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // Define the path to your SQL file
+  const sqlFilePath = path.join(__dirname, 'sql', 'seed_data.sql');
+  
+  // Read the SQL file
+  const sql = fs.readFileSync(sqlFilePath, 'utf-8');
+
+  // Split the SQL commands if you have multiple commands
+  const sqlCommands = sql.split(';').filter(command => command.trim());
+
+  // Execute each command
+  for (const command of sqlCommands) {
+    await prisma.$executeRawUnsafe(command);
+  }
+
+  console.log(`Seeding completed!`);
+}
+
+main()
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
