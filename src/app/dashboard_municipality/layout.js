568ad6e2df23ed35a37850f77575d3949dfe7eb9
saran <6431503063@lamduan.mfu.ac.th>
2024-11-05 13:32:41 +0700

    ลืมๆ

---
+++ src/app/dashboard_municipality/layout.js
@@ -0,0 +1,25 @@
+// components/Layout.js
+"use client";
+import { useSession } from "next-auth/react";
+import Sidebar from "../ui/dashboard/sidebarMunicipality/sidebar";
+import Navbar from "../ui/dashboard/navbar/navbar";
+
+const Layout = ({ children }) => {
+  const { data: session } = useSession();
+  const isAdmin = session?.user?.role === "admin";
+
+  return (
+    <div>
+      {/* Navbar at the top */}
+      <Navbar />
+
+      {/* Sidebar and Main Content */}
+      <div className="flex pt-[60px] xl:space-x-[350px]">
+        {!isAdmin && <Sidebar />}
+        <main className="ml-[330px] flex-1 pr-4">{children}</main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/app/dashboard_municipality/page.js
@@ -0,0 +1,34 @@
+
+import dynamic from "next/dynamic"
+import styles from "../ui/dashboard/dashboard.module.css"
+
+
+const Card = dynamic(() => import("../ui/dashboard/card/card"),
+  {
+    loading: () => <p>Loading...</p>,
+  }
+)
+
+const Chart = dynamic(() => import("../ui/dashboard/chart/chart"))
+const Transactions = dynamic(() => import("../ui/dashboard/transactions/transactions"))
+
+
+const Dashboard = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <div>
+          <Transactions />
+        </div>
+        <Chart />
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
