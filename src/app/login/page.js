a1504b8cbf649222d75966519bc3c669819e1793
DARANPHOP SAMUTKAO <6431503022@lamduan.mfu.ac.thcd>
2024-11-28 00:17:06 +0700

    updated cloud image delete and update

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -4,7 +4,7 @@ import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 
-export default function LoginPage() {
+export default function Login() {
   const router = useRouter()
   const [identifier, setIdentifier] = useState('')
   const [password, setPassword] = useState('') 
